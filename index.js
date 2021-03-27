42d936b5cc1af5346a04669094404664aaad2a05
Oscar Askfelt <oscaraskfelt@gmail.com>
2021-03-27 14:35:44 +0100

    install socket and intial setup with html-template

--- index.js
+++ index.js
@@ -1,15 +1,30 @@
-const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const path = require('path');
 
-const app = express();
+const app = require('express')();
+const http = require('http').createServer(app);
+const io = require('socket.io')(http);
+
 app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get('/', (req, res, next) => {
-  res.status(200).send('Hejsan');
+  res.status(200).sendFile(path.join(__dirname + '/views/index.html'));
+});
+
+io.on('connection', (socket) => {
+  console.log('connected');
+
+  io.emit('message', {
+    data: 'example data',
+  });
+
+  socket.on('disconnect', () => {
+    console.log('disconnected');
+  });
 });
 
 const port = process.env.PORT || 5000;
-app.listen(port, () => console.log(`Server running on ${port}`));
+http.listen(port, () => console.log(`Server running on ${port}`));
