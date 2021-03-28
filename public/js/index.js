94a85f52449deccd8336acc9ca95af394f07f888
Oscar Askfelt <oscaraskfelt@gmail.com>
2021-03-28 20:13:58 +0200

    email validation + some index styling

--- public/js/index.js
+++ public/js/index.js
@@ -1,15 +1,37 @@
 $(document).ready(function () {
+  //Handling form for registering/logging in a user
   $('#registerForm').on('submit', (e) => {
     const id = $('#registerEmail').val();
     sessionStorage.setItem('user', id);
+
+    if (
+      !/^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/.test(
+        id
+      )
+    ) {
+      console.log('fail');
+      e.preventDefault();
+      $('#registerForm').append(
+        '<p class="error">Det måste vara en giltig e-post</p>'
+      );
+    }
   });
 
+  //Handling form for unregistering a user
   $('#removeForm').on('submit', (e) => {
     e.preventDefault();
+
     const id = $('#removeEmail').val();
     sessionStorage.removeItem('user');
+
     $.post('/removeUser', { id: id }, (response) => {
-      console.log(response);
-    }).fail((err) => console.log(err));
+      $('.valid').empty();
+      $('.error').empty();
+      $('#removeForm').append('<p class="valid">E-post borttagen</p>');
+    }).fail((err) => {
+      $('.error').empty();
+      $('.valid').empty();
+      $('#removeForm').append('<p class="error">E-posten fanns inte</p>');
+    });
   });
 });
